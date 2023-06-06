1a02b051a0fc8540efc63de4b02472d186a3fce5
Çetin Ataman <cetin.ataman@ibtech.com.tr>
2023-06-06 14:30:03 +0300

    init

---
+++ android/app/src/main/kotlin/com/text/myapp/my_test_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.text.myapp.my_test_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/NotificationServiceExtension/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  NotificationServiceExtension
+//
+//  Created by Cetin on 6.06.2023.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
